4269422e658bcbdb39ba1af7b7f71c2fe1889a5d
raf0c <ragampo@gmail.com>
2016-08-14 17:11:25 -0500

    Creation of our custom WelcomeView layout to abstract our view.

--- app/src/main/java/com/example/raf0c/fallingwordsgame/MainActivity.java
+++ app/src/main/java/com/example/raf0c/fallingwordsgame/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.raf0c.fallingwordsgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.raf0c.fallingwordsgame.views.WelcomeView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WelcomeView welcomeView = (WelcomeView) findViewById(R.id.welcome_layout);
     }
 }
---
+++ app/src/main/java/com/example/raf0c/fallingwordsgame/views/WelcomeView.java
@@ -0,0 +1,21 @@
+package com.example.raf0c.fallingwordsgame.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by raf0c on 14/08/16.
+ * Class for abstracting the view of our Welcome Screen
+ */
+public class WelcomeView extends RelativeLayout {
+
+    public WelcomeView(Context context) {
+        super(context);
+    }
+
+    public WelcomeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
